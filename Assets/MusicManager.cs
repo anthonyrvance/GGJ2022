c27e7b41d94c51fa3f34fa120e31c0d6a6fd6f4d
gmeisinger <gmeisinger@gmail.com>
2022-01-29 17:14:20 -0600

    added music

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
